f23f0b17f8c8cf4286e67581c06b20e5f118cf54
Sudiksha Vaidyanathan <sudikshavaidy@gmail.com>
2024-07-16 14:01:39 -0700

    frontend plus deployment

--- backend/app.py
+++ backend/app.py
@@ -1,39 +1,35 @@
-#ok, so before we installed flask or anything, we created a virtual environment using python -m venv venv, where the second venv is the name of our virtual environment 
-# this allowed us to install flask without having to do it on our machine-- instead it is only on the venv 
-
-
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
-from flask_cors import CORS 
-
+# TODO: UPDATE THIS FILE FOR DEPLOYMENT
+from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app)
+
+# We can comment this CORS config for the production because we are running the frontend and backend on the same server
+# CORS(app) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+# Server static files from the "dist" folder under the "frontend" directory
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
+
+# api routes
 import routes
 
-#the below creates the tables needed, creates context for optimization 
-
-with app.app_context(): 
-    db.create_all()
-
-
-#what this if statement below does is only if app.py is ran directly using python app.py, the entire thing will run on its own
-# if app.py is imported into a a diff file like it is in models.py. using 'from app import db'
-# we dont want the entirety of app.py to run multiple times -- only once
-# so th if statement prevents infinite loops 
-
-#snce running on a venv, of u go to localhost5000/api/friends and it says no tbale friends, make sure to 
-# set FLASK_APP=app.py and set FLASK_ENV=development and flask run
-#do all 3 commands in terminal
-# ACTUALLY instead of flask run, do flask run --reload to avoid having to reload 
-
-
+with app.app_context():
+  db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
